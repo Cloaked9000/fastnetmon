39e8c365c6a6741f1fbfeb46ec09637070a7d501
Fred Nicolson <fred.nicolson@outlook.com>
2016-03-10 11:23:42 +0000

    Test

--- src/fastnetmon.cpp
+++ src/fastnetmon.cpp
@@ -225,9 +225,6 @@ bool exabgp_flow_spec_announces = false;
 ban_settings_t global_ban_settings;
 
 void init_global_ban_settings() {
-    //Flag for detecting global settings
-    global_ban_settings.is_global_ban_settings = true;
-
     // ban Configuration params
     global_ban_settings.enable_ban_for_pps = false;
     global_ban_settings.enable_ban_for_bandwidth = false;
@@ -2258,9 +2255,9 @@ void recalculate_speed() {
                     logger << log4cpp::Priority::INFO << convert_ip_as_uint_to_string(client_ip) << " is whitelisted and so not added to the ban list.";
                 }
             }
-            else if(current_ban_settings.is_global_ban_settings == false)
+            else
             {
-                logger << log4cpp::Priority::INFO << convert_ip_as_uint_to_string(client_ip) << " whitelisted. Limit: " << current_ban_settings.ban_threshold_udp_pps;
+                logger << log4cpp::Priority::INFO << convert_ip_as_uint_to_string(client_ip) << " whitelisted. Limit: " << current_ban_settings.ban_threshold_pps;
                 //IP shouldn't be banned and so add to whitelist
                 ip_whitelist.push_back(client_ip);
 
